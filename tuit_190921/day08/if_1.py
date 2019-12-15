fb57230e8f37789a77e65dda1f5ea2a2206f0a47
Athens <athens314@hotmail.com>
2019-12-14 20:59:17 -0500

    added day08

---
+++ tuit_190921/day08/if_1.py
@@ -0,0 +1,12 @@
+# if-else statement
+
+has_pocket_money = False
+
+if has_pocket_money:
+    print("Yeah, I am going out for shopping.")
+    print("Then, I will go to the restaurant.")
+    print("After that, I will go the cinema.")
+
+print("bye bye")
+
+
