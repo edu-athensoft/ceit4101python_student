232d4ae44b6fd442f35400ab8e2059e3f5e71d14
Athens <athens314@hotmail.com>
2020-02-12 13:14:43 -0500

    added 191221 class for zhou

---
+++ ceit_191221/py200212_zhou/keywords_2.py
@@ -0,0 +1,88 @@
+"""
+keywords of Python
+"""
+
+"""
+False
+
+True
+
+None
+
+and
+
+as
+
+assert
+
+break
+
+class
+
+continue
+
+def
+
+del 
+
+elif
+
+else
+
+except
+
+finally
+
+for
+
+from
+
+global
+
+if
+
+import 
+
+in 
+
+is
+
+lambda
+
+nonlocal
+
+not
+
+or
+
+pass
+
+raise
+
+return
+
+try
+
+while
+
+with
+
+yield
+"""
+
+"""
+True, False, None
+and, or, not
+if, else, elif, for, while, break, continue
+def, return, yield, lambda, pass
+class
+try, except, raise, finally, with
+global, nonlocal
+import, from, as
+assert, del, in, is
+"""
+
+# what if type False as false?
+# not recommended
+false = 99
+print(false)
---
+++ ceit_191221/py200212_zhou/statement_3.py
@@ -0,0 +1,32 @@
+"""
+multi-line statement
+"""
+
+# one statement spans multiple lines
+sum = 1 + 2 + 3 + 4 + \
+1 + 2 + 3 + 4 + \
+1 + 2 + 3 + 4
+
+
+# one statement spans multi-lines using braces
+# case 1 - ()
+a = (1 + 2 +
+     3 + 4)
+
+# case 2 - []
+b = [1, 2, 3,
+     4, 5, 6]
+b2 = [[1, 2, 3],
+     [4, 5, 6]]
+b2 = [[1, 2, 3],[4, 5, 6]]
+
+# case 3 - {}
+c = {"a":1,
+     "b":2}
+
+
+
+
+
+
+
